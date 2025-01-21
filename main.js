19f60d66ec6847abf211407c6f53ebbcf82b09f5
takaza-codes <elekovalenko@gmail.com>
2025-01-21 22:55:17 +0300

    add js

---
+++ main.js
@@ -0,0 +1,66 @@
+const nameField = document.getElementById('userName');
+const checkedYes = document.getElementById('yes');
+
+function showName () {
+    if (checkedYes.checked) {
+        nameField.innerHTML = `
+        <label> Введите ваше имя:<br>
+        <input type="text" name="username" id="enteredName">
+        </label>
+        `;
+    } else {
+        nameField.innerHTML = '';
+    }
+}
+
+function formatName (name) {
+        name = name.trim();
+        const firstLetter = name[0].toUpperCase();
+        const followingLetters = name.slice(1).toLowerCase();
+        return firstLetter + followingLetters;
+    }
+
+function checkSpam (str) {
+    const message = str.toLowerCase();
+    if (message.includes('viagra')) {
+        str = str.replace(/viagra/gi, '***');
+    }
+    if (message.includes('xxx')) {
+        str = str.replace(/xxx/gi, '***');
+    }
+    return str;
+}
+
+const submitButton = document.getElementById('submitButton');
+const commentSection = document.getElementById('commentSection');
+
+function submit() {
+    const nameInput = document.getElementById('enteredName');
+    let name;
+
+    if (nameInput) {
+        name = nameInput.value.trim() ? formatName(nameInput.value) : 'username';
+    } else {
+        name = 'username';
+    }
+
+    const avatarInput = document.getElementById('imageURL');
+    let avatar = avatarInput.value.trim() ? avatarInput.value.trim() : 'https://images.vexels.com/media/users/3/325213/isolated/preview/c52fc3523ce9a94e0a75edeb1d806c9e-yellow-smiley-face.png';
+
+
+    const messageInput = document.getElementById('comment');
+    let message = checkSpam(messageInput.value.trim());
+
+    const comment = document.createElement('div');
+    comment.className = 'newComments';
+    comment.innerHTML = `
+    <img src="${avatar}" alt="Аватар" class="avatar";
+    <span class="name">${name}</span>
+    <div class="commentText">${message}</div>
+    `;
+
+    commentSection.appendChild(comment);
+    if (nameInput) {nameInput.value = ''};
+    avatarInput.value = '';
+    messageInput.value = '';
+}
